2e22ca22aa7bff7bff9662c493ac964d4446cde7
Maha <Memo1992mm2010@hotmail.com>
2021-12-15 08:53:11 +0300

    Initial Commit

---
+++ TodoList App/TodoTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TodoTableViewCell.swift
+//  TodoList App
+//
+//  Created by Maha saad on 10/05/1443 AH.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var todoTitle: UILabel!
+    @IBOutlet weak var todoDueDate: UILabel!
+    @IBOutlet weak var todoNotes: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
